250901b73d7857f982455b0e24f5c9eddcaa9ccd
choojunyup <choojun12@naver.com>
2020-05-24 22:39:08 +0900

    error edit

--- js/game.js
+++ js/game.js
@@ -25,7 +25,7 @@
         var backImg;
         var cardsmaskImg;
         var cardmaskImg;
-        var cardColors = ["Black", "Heart", "Club", "Diamond"];
+        var cardColors = ["black", "heart", "club", "diamond"];
         var cardIndexs = ["_A", "_2", "_3", "_4", "_5", "_6", "_7", "_8", "_9", "_10", "_J", "_Q", "_K"];
         var canvasBack = document.getElementById("canvasBack");
         var canvasFont = document.getElementById("canvasFont");
